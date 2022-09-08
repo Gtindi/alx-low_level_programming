c0a845793aadc99be7c92187ca14929f4aec877c
Gtindi <tindigabriel@gmail.com>
2022-09-08 16:21:43 +0300

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /***
- * main - Is the entry point
+ * main - Entry point
  *
- * Return: Must always be 0(success)
+ * Return:Always 0(success)
  *
  * This is the Betty coding style. I wonder who Betty is though. I would Definetely date her.
  *
  */
-
 int main(void){
   char message[] = "\"Programming is like building a multilingual puzzle";
   puts(message);
