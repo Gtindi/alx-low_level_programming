caf7fa2969e93dd4d3bf480a11d68bb5816bed9e
Gtindi <tindigabriel@gmail.com>
2022-09-08 16:27:08 +0300

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return - Always return 0 (Success)
+ *
+ */
+int main{
+  char message = "\with proper grammar, but the outcome is a piece of art,";
+  printf(message)
+    return 0
+}
