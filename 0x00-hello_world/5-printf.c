8bd05ae0213a71169039792212a5ae70f48c52ce
Gtindi <tindigabriel@gmail.com>
2022-09-08 16:29:09 +0300

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,7 @@
  * Return - Always return 0 (Success)
  *
  */
-int main{
-  char message = "\with proper grammar, but the outcome is a piece of art,";
-  printf(message)
+int main(void){
+  printf("with proper grammar, but the outcome is a piece of art,\n")
     return 0
 }
