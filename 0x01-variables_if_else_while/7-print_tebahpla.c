971c6173cad0c477610b72b567c6dacd0615f854
Gtindi <tindigabriel@gmail.com>
2022-09-09 18:45:46 +0300

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char ch = 'a';
+int len = ch + 26;
+
+while (ch < len)
+{
+character = ch++;
+gets(s);
+strrev(s);
+putchar(character, s);
+
+}
+putchar('\n');
+return (0);
+}
