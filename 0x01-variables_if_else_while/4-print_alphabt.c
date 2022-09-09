56604f04f3d9f2b43a576f2325913a4a8f101a81
Gtindi <tindigabriel@gmail.com>
2022-09-09 17:57:52 +0300

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char ch = 'a';
+int len = ch + 26;
+
+while (ch < len)
+{
+if (c == 'q' || c == 'e')
+{
+++c;
+continue;
+}
+putchar(ch++);
+
+}
+putchar('\n');
+return (0);
+}
