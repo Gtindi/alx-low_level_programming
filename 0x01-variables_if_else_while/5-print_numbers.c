367a686555dfb2f4778fe33a39c1089ac765fb14
Gtindi <tindigabriel@gmail.com>
2022-09-09 18:03:37 +0300

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int a = 0;
+while (a < 10)
+{
+a++
+printf(a);
+}
+
+return (0);
+}
