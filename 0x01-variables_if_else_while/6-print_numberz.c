74467f60d1651d1bd6236cb0d5351523e75466ee
Gtindi <tindigabriel@gmail.com>
2022-09-09 18:23:16 +0300

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int a = 0;
+while (a < 10)
+{
+putchar("%d", a);
+a += 1;
+}
+putchar('\n');
+return (0);
+}
