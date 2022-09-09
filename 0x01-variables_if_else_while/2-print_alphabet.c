50072e33d54efb83b80d29307479bbc5a0f89ecb
Gtindi <tindigabriel@gmail.com>
2022-09-09 15:36:00 +0300

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char ch = 'a';
+
+while (ch <= 'z')
+{
+putchar("%c\n", ch);
+ch++
+}
+return (0);
+}
