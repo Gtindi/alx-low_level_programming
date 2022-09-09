76032af8b0be76c85ba52f0a34312c07a034a5d2
Gtindi <tindigabriel@gmail.com>
2022-09-09 19:00:55 +0300

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int i = 0;
+
+while (i <= 15)
+{
+if (i < 10)
+putchar(i + '0');
+else
+putchar(i - 10 + 'a');
+i++;
+}
+putchar('\n');
+return (0);
+}
