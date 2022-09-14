c29a1028d7f487c3cbabad32fd7db69580c5eb80
Gtindi <tindigabriel@gmail.com>
2022-09-14 23:13:39 +0300

    Task 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase to stdout
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+int i = 0;
+int start;
+int max;
+
+while (i < 10)
+{
+start = 97;
+max = start + 26;
+while (start < max)
+{
+  _putchar(start++);
+}
+_putchar('\n');
+}
+}
