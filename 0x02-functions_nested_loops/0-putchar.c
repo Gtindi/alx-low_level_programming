312aca5f099c05502dca8222d52ffbee996d20bb
Gtindi <tindigabriel@gmail.com>
2022-09-14 16:18:17 +0300

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
- *main:Entry point
+ * main - Entry point
  *
- * Return - Always 0
- **/
+ * Return: Always 0
+ */
 int main(void)
 {
-
+char x = "_putchar";
+printf(x);
+return (0);
 }
