d94a8f7404551fcf8b3cf890b07bc317f60fdc1f
Gtindi <tindigabriel@gmail.com>
2022-09-14 20:01:25 +0300

    Task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main"
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int alpha = 97;
+int max = 97 + 26;
+
+while (alpha < max)
+{
+_putchar(alpha++);
+}
+_putchar('\n');
+return (0);
+}
