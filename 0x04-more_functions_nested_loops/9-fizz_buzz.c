c587106b376e0f97950e385040d92bace1ac0d71
Gabriel Tindi <tindigabriel@gmail.com>
2022-09-16 00:03:26 +0300

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main- program that prints either no. , fizz, buzz or fizzbuzz
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int num = 1;
+
+	while (num++ < 100)
+	{
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((num % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((num % 5) == 0)
+		{
+			if (num != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
