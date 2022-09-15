b06affd7e6c20cbc5a3a7e218bbb90862f6ce272
Gabriel Tindi <tindigabriel@gmail.com>
2022-09-16 00:17:17 +0300

    Advanced Task 2

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,16 +8,66 @@
 
 void print_number(int n)
 {
-	unsigned int num = n;
+	int digit, divisor;
+	int base = 9;
+	int start = 0;
+	int isNegative = 0;
+	int isMinInt = 0;
 
 	if (n < 0)
 	{
-		putchar('-');
-		num = -num;
+		isNegative = 1;
+		n = n * -1;
+		isMinInt = n < 0 ? 1 : 0;
+		n = n < 0 ? (n + 1) * -1 : n;
 	}
-	if (num > 9)
+	while (base >= 0)
 	{
-		print_number(num / 10);
+		divisor = to_power(10, base);
+		digit = ((n / divisor) % 10);
+		if (start == 0 && digit == 0)
+		{
+			base--;
+		}
+		else if (start == 0 && digit != 0)
+		{
+			base--;
+			start = 1;
+			if (isNegative == 1)
+				_putchar('-');
+			_putchar(digit + '0');
+		}
+		else
+		{
+			if (isMinInt && divisor == 1)
+				_putchar((digit + 1) + '0');
+			else
+				_putchar(digit + '0');
+			base--;
+		}
 	}
-	putchar(num % 10 + '0');
+	if (divisor == 1 && start == 0)
+		_putchar(48);
 }
+
+/**
+ * to_power - Computes the exponential of a number
+ * @n: The number
+ * @base: The power that n is raised to
+ *
+ * Return: The computed power
+ */
+
+int to_power(int n, int base)
+{
+	int i = 0;
+	int exp = 1;
+
+	while (i < base)
+	{
+		exp *= n;
+		i++;
+	}
+	return (exp);
+}
+
