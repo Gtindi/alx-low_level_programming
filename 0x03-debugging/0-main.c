d8a7f01b2e47865afbdb366f72975c09d94aafe6
Gabriel Tindi <tindigabriel@gmail.com>
2022-09-16 00:19:51 +0300

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main- tests that prints if integer is +ve or -ve
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
