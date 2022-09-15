b6282f366f115d0b4d183def8167897dfe4d711d
Gabriel Tindi <tindigabriel@gmail.com>
2022-09-15 23:48:36 +0300

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,23 +9,23 @@
 
 void print_square(int size)
 {
-	int inc1, inc2;
+int inc1, inc2;
 
-	if (size > 0)
-	{
-		for (incl = 0; incl < size; incl++)
-		{
-			for (inc2 = 0; inc2 < (size - 1); inc2++)
-			{
-				putchar('#');
-			}
+if (size > 0)
+{
+for (incl = 0; incl < size; incl++)
+{
+for (inc2 = 0; inc2 < (size - 1); inc2++)
+{
+putchar('#');
+}
 
-			putchar('#');
-			putchar('\n');
-		}
-	}
-	else
-	{
-		putchar('\n');
-	}
+putchar('#');
+putchar('\n');
+}
+
+else
+{
+putchar('\n');
+}
 }
