d1899093ff6f18569750b7b2dce17c36a00af6b0
Gtindi <tindigabriel@gmail.com>
2022-09-19 21:00:32 +0300

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef _MAIN_
 #define _MAIN_
-int _putchar(har c);
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -13,4 +13,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 int _isdigit(int c);
-#endifc
+#endif
