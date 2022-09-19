1bfeaf6c188e8f21431e864831985db9d8501ecd
Gtindi <tindigabriel@gmail.com>
2022-09-19 20:58:32 +0300

    Task 4

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,30 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-
-int _isupper(int c);
-
+#ifndef _MAIN_
+#define _MAIN_
+int _putchar(har c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 int _isdigit(int c);
-
-int mul(int a, int b);
-
-void print_numbers(void);
-
-void print_most_numbers(void);
-
-void more_numbers(void);
-
-void print_line(int n);
-
-void print_diagonal(int n);
-
-void print_square(int size);
-
-void print_triangle(int size);
-
-void print_number(int n);
-
-int to_power(int n, int base);
-
-#endif
+#endifc
