7699d53a447839315ea4d9a8f0842edfa534feac
Gtindi <tindigabriel@gmail.com>
2022-09-19 20:29:32 +0300

    README Update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 - Updates n to 98
+ * @n: The number to be updated
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+<h1> Pointers arrays strings </h1>
