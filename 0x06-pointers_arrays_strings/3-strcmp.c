9d8fe428cc45126ab3425cbd609fb1ede5419e86
MarieLisah <lisaw.maria@gmail.com>
2022-09-22 23:26:46 +0300

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings.
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+int res, i, len1, len2;
+
+len1 = _strlen(s1);
+len2 = _strlen(s2);
+res = 0;
+i = 0;
+
+while (s1[i] != '\0')
+{
+if (s1[i] != s2[i])
+{
+res = (int)s1[i] - (int)s2[i];
+break;
+}
+i++;
+}
+
+if (res == 0 && len1 != len2)
+{
+res = s2[i] * -1;
+}
+return (res);
+}
+
+/**
+ * _strlen - This function returns the length of a string.
+ * @s: The string to return the length
+ *
+ * Return: int
+ */
+int _strlen(char *s)
+{
+long int i = 0;
+
+while (s[i] != '\0')
+i++;
+return (i);
+}
