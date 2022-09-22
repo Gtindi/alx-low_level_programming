51c3f9bcc3e8fac12d1f0148fd2a014608ca0ae7
MarieLisah <lisaw.maria@gmail.com>
2022-09-22 23:27:42 +0300

    main.h

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_
+#define _MAIN_
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+int is_delimiter(char c);
+int _islower(int c);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+int to_power(int n, int base);
+#endif
