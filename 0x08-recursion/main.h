621e4fc3e019ed02fb6869d73c38b92325edd635
Gtindi <tindigabriel@gmail.com>
2022-09-28 16:53:00 +0300

    Task 0

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_
+#define _MAIN_
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _isqrt(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int _is_prime(int n, int x);
+int is_palindrome(char *s);
+int _strlen(char *s);
+int _compare(char *s, int start, int end);
+int wildcmp(char *s1, char *s2);
+#endif
