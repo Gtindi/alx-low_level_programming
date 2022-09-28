4a0bec3d928b9e3c51fc7f6512325a1f5cc3ade4
Gtindi <tindigabriel@gmail.com>
2022-09-28 17:43:15 +0300

    Task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,33 +1,30 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings and returns 1 if the strings can be
- * considered identical, otherwise return 0.
- * @s1: The first string
- * @s2: The second string
- *
- * Return: int
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
  */
+
 int wildcmp(char *s1, char *s2)
 {
-	int res;
+if (*s1 == '\0')
+{
+if (*s2 != '\0' && *s2 == '*')
+{
+return (wildcmp(s1, s2 + 1));
+}
+return (*s2 == '\0');
+}
 
-	if (*s1 == '\0' && *s2 == '\0')
-		res = 1;
-	else if (*s1 != *s2 && *s2 != '*')
-		res = 0;
-	else if (*s1 == '\0' && *s2 == '*')
-		res = wildcmp(s1, ++s2);
-	else if (*(s1 + 1) == '\0' && *(s2 + 1) != '\0')
-		res = wildcmp(s1, ++s2);
-	else if (*s1 == *s2 && *(s2 + 1) == '*')
-		res = wildcmp(s1, ++s2);
-	else if (*(s1 + 1) != *(s2 + 1) && *(s2 + 1) == '*')
-		res = wildcmp(++s1, ++s2);
-	else if (*(s1 + 1) != *(s2 + 1) && *s2 == '*')
-		res = wildcmp(++s1, s2);
-	else
-		res = wildcmp(++s1, ++s2);
-
-	return (res);
+if (*s2 == '*')
+{
+return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+}
+else if (*s1 == *s2)
+{
+return (wildcmp(s1 + 1, s2 + 1));
+}
+return (0);
 }
