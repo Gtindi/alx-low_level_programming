641dfd9d96da1e201818a46e58efe8e4426e30fa
Gtindi <tindigabriel@gmail.com>
2022-10-06 22:22:16 +0300

    Advanced Task 2

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,138 +1,126 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 #include <ctype.h>
 
 /**
- * main - a program that multiplies two positive numbers.
- * @argc: The argument count
- * @argv: The arguments
+ * _is_zero - determines if any number is zero
+ * @argv: argument vector.
  *
- * Return: 0 on success, 38 otherwise
+ * Return: no return.
+ */
+void _is_zero(char *argv[])
+{
+	int i, isn1 = 1, isn2 = 1;
+
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
+		{
+			isn1 = 0;
+			break;
+		}
+
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
+		{
+			isn2 = 0;
+			break;
+		}
+
+	if (isn1 == 1 || isn2 == 1)
+	{
+		printf("0\n");
+		exit(0);
+	}
+}
+
+/**
+ * _initialize_array - set memery to zero in a new array
+ * @ar: char array.
+ * @lar: length of the char array.
+ *
+ * Return: pointer of a char array.
+ */
+char *_initialize_array(char *ar, int lar)
+{
+	int i = 0;
+
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
+	ar[lar] = '\0';
+	return (ar);
+}
+
+/**
+ * _checknum - determines length of the number
+ * and checks if number is in base 10.
+ * @argv: arguments vector.
+ * @n: row of the array.
+ *
+ * Return: length of the number.
+ */
+int _checknum(char *argv[], int n)
+{
+	int ln;
+
+	for (ln = 0; argv[n][ln]; ln++)
+		if (!isdigit(argv[n][ln]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+
+	return (ln);
+}
+
+/**
+ * main - Entry point.
+ * program that multiplies two positive numbers.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ *
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-	int mul[2000] = {0};
-	int i, j;
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
+	char *nout;
 
 	if (argc != 3)
+		printf("Error\n"), exit(98);
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	if (nout == NULL)
+		printf("Error\n"), exit(98);
+	nout = _initialize_array(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	for (; k >= 0; k--, i--)
 	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	for (i = 0; i < 1024; i++)
-		mul[i] = 0;
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		if (i < 0)
 		{
-			if (!isdigit(argv[i][j]))
+			if (addl > 0)
 			{
-				printf("Error\n");
-				exit(98);
+				add = (nout[k] - '0') + addl;
+				if (add > 9)
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
 			}
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+		}
+		if (j < 0)
+		{
+			if (nout[0] != '0')
+				break;
+			lnout--;
+			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+		}
+		if (j >= 0)
+		{
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
 		}
 	}
-	_multiply(mul, argv);
-	print_ans(mul);
+	printf("%s\n", nout);
 	return (0);
-}
-
-/**
- * _strlen - This function returns the length of a string.
- * @s: The string to return the length
- *
- * Return: Length of s
- */
-int _strlen(char *s)
-{
-	long int len = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	return (len);
-}
-
-/**
- * _multiply - multiply two numbers
- * @mul: The array to store the result
- * @str: The string
- *
- * Returns: void
- */
-void _multiply(int *mul, char *str[])
-{
-	int i, j, len1, len2, tmp;
-	char *s1 = str[1];
-	char *s2 = str[2];
-	rev_string(s1);
-	rev_string(s2);
-
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	for (i = 0; i < len1; i++)
-	{
-		for (j = 0; j < len2; j++)
-		{
-			mul[i + j] += (s1[i] - '0') * (s2[j] - '0');
-		}
-	}
-
-	for (i = 0; i < len1 + len2; i++)
-	{
-		tmp = mul[i] / 10;
-		mul[i] = mul[i] % 10;
-		mul[i + 1] = mul[i + 1] + tmp;
-	}
-}
-
-/**
- * rev_string - This function reverses a string.
- * @s: the string to be reversed
- *
- * Return: void
- */
-void rev_string(char *s)
-{
-	char tmp1, tmp2;
-	int i, j;
-
-	i = _strlen(s) - 1;
-	j = 0;
-
-	while (i != j && j < i)
-	{
-		tmp1 = s[i];
-		tmp2 = s[j];
-		s[i] = tmp2;
-		s[j] = tmp1;
-		j++;
-		i--;
-	}
-}
-
-/**
- * print_ans - Print the answer
- * @mul: the product
- *
- * Return: void
- */
-void print_ans(int *mul)
-{
-	int i, isZero;
-
-	isZero = 1;
-	for (i = 1023; i >= 0; i--)
-	{
-		if (mul[i] > 0)
-		{
-			isZero = 0;
-			break;
-		}
-	}
-	for (; i >= 0; i--)
-		printf("%d", mul[i]);
-	if (isZero == 1)
-		printf("0");
-	printf("\n");
 }
