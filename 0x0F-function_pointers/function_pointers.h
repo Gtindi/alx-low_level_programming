370b24fe84452367417fda0912cc07118ce6eaf8
Gtindi <tindigabriel@gmail.com>
2022-10-13 23:27:16 +0300

    Task 0

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef _FUNCTION_H_
+#define _FUNCTION_H_
+#include <stdio.h>
+#include <ctype.h>
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+void print_opcode(char *op, int n);
+#endif
