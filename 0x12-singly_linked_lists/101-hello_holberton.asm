76c76b12fb987388c81594b030bb516ac59855a6
Gtindi <tindigabriel@gmail.com>
2022-10-25 15:27:40 +0300

    Repo Updated

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,8 +1,26 @@
-#include <stdio>
+; File: 101-hello_holberton.asm
+; Auth: Muchunu Linet
+; Desc: 64-bit assembly program that prints
+;       Hello, Holberton followed by a new line.
 
-int main(void)
-{
-	printf("Hello, Holberton\n");
+extern printf
 
-	return (0);
-}
+section .text
+   global main
+
+main:
+   push rbp
+
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
+
+   pop rbp
+
+   mov rax,0
+   ret
+
+section .data
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
